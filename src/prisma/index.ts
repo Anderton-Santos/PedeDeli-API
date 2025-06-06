213785fac9230c3ef9da3052d5f7a5206e73f1f3
Anderton Santos <anderton9814@outlook.com>
2025-06-06 15:14:14 -0300

    Ajustando para deploy

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "../generated/prisma/client";
-// import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 
 const prismaClient = new PrismaClient()
